2356859c0b83ea0a53dd0ebd3764ead9247f9d14
j-katzen <alto50@gmail.com>
2017-03-21 03:31:21 -0700

    comment code

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var ProfitChecker = require('./lib/ProfitChecker');
 
 // var minerHandle = new MinerHandler();
 
+// setup hashes based on their uri for whattomine; will eventually
+// create a map to auto-translate from cointags
 const algoHashes = {
   eth: 900,
   equihash: 7000,
   bk14: 4350
 };
 
-var checker = new ProfitChecker(algoHashes, ['ETC', 'ETH', 'ZEC', 'UBQ', 'DCR', 'EXP']);
+// pass algoHashes object and array of cointags to pick from. omitted ETH as good test
+var checker = new ProfitChecker(algoHashes, ['ETC', 'ZEC', 'UBQ', 'DCR', 'EXP']);
 
+// log out the max coin
 checker.getCoinProfitability().then(function(maxProfitCoin) {
   console.log(maxProfitCoin);
 });
