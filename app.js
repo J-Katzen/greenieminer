918b72c2097cdfbd5dfef8f628dbfb8834844977
j-katzen <alto50@gmail.com>
2017-03-21 03:29:52 -0700

    create profitchecker and create test app.js file to run it against

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var MinerHandler = require('./lib/MinerHandler');
 var ProfitChecker = require('./lib/ProfitChecker');
 
-var minerHandle = new MinerHandler();
+// var minerHandle = new MinerHandler();
+
+const algoHashes = {
+  eth: 900,
+  equihash: 7000,
+  bk14: 4350
+};
+
+var checker = new ProfitChecker(algoHashes, ['ETC', 'ETH', 'ZEC', 'UBQ', 'DCR', 'EXP']);
+
+checker.getCoinProfitability().then(function(maxProfitCoin) {
+  console.log(maxProfitCoin);
+});
 
