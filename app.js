713a23c21d628ccf96e3d456e8437117316c84d2
j-katzen <alto50@gmail.com>
2017-03-21 21:05:12 -0700

    create function to get the coins associated with an algorithm

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ var ProfitChecker = require('./lib/ProfitChecker');
 // setup hashes based on their uri for whattomine; will eventually
 // create a map to auto-translate from cointags
 const algoHashes = {
-  eth: 900,
-  equihash: 7000,
-  bk14: 4350
+  ethash: 900.0,
+  equihash: 7000.0,
+  blake14r: 4350.0,
+  lyra2rev2: 2000
 };
 
 // pass algoHashes object and array of cointags to pick from. omitted ETH as good test
-var checker = new ProfitChecker(algoHashes, ['ETC', 'ZEC', 'UBQ', 'DCR', 'EXP']);
+var checker = new ProfitChecker(algoHashes, ['UBQ', 'DCR', 'ZEC', 'ETH', 'ETC', 'VTC']);
 
 // log out the max coin
-checker.getCoinProfitability().then(function(maxProfitCoin) {
-  console.log(maxProfitCoin);
-});
+// checker.getCoinProfitability().then(function(maxProfitCoin) {
+//   console.log(maxProfitCoin);
+// });
 
+checker.getCoinsByAlgo('blakexvc').then(function(matchingCoins) {
+  console.log(matchingCoins);
+});
